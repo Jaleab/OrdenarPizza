e2f8f3152cec2cdc98e207305b9085ae8c9df4bd
Javier Abarca Jimenez <javier.abarca@ucr.ac.cr>
2020-07-07 18:50:40 -0600

    Ingresar direccion mediante modal

--- OrdenarPizza/Models/FacturaModel.cs
+++ OrdenarPizza/Models/FacturaModel.cs
@@ -1,11 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace OrdenarPizza.Models
 {
     public class FacturaModel
     {
+        [Required(ErrorMessage = "*El cantón es requerido.")]
+        [DisplayName("Cantón")]
+        public string canton { get; set; }
+
+        [Required(ErrorMessage = "*La provincia es requerida.")]
+        [DisplayName("Provincia")]
+        public string provincia { get; set; }
+
+        [Required(ErrorMessage = "*El número de teléfono es requerido.")]
+        [DisplayName("Número de teléfono")]
+        public string numeroTelefónico { get; set; }
+
+        [DisplayName("Total:")]
+        public string costoTotal { get; set; }
+
+        [DisplayName("Impuesto:")]
+        public string costoImpuesto { get; set; }
+
+        [DisplayName("Subtotal:")]
+        public string subtotal { get; set; }
     }
 }
