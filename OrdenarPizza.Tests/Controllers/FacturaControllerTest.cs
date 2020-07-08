398e7be9f73419c78f6be902682f0e4a2b4f5dce
Javier Abarca Jimenez <javier.abarca@ucr.ac.cr>
2020-07-08 11:58:17 -0600

    Pruebas unitarias de no es null /redireccionado en Pizza y Factura

---
+++ OrdenarPizza.Tests/Controllers/FacturaControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrdenarPizza.Controllers;
+using OrdenarPizza.Models;
+using OrdenarPizza.Services;
+using System.Web.Mvc;
+
+namespace OrdenarPizza.Tests.Controllers
+{
+    [TestClass]
+    public class FacturaControllerTest
+    {
+        [TestMethod]
+        public void VerFacturaIsNotNull()
+        {
+            PizzaModel pizza = new PizzaModel();
+            FacturaService servicioFactura = new FacturaService();
+            FacturaModel factura = new FacturaModel();
+
+            FacturaController controller = new FacturaController();
+
+            ViewResult resultado = controller.VerFactura(pizza) as ViewResult;
+
+            Assert.IsNotNull(resultado);
+        }
+
+        [TestMethod]
+        public void CompraRealizadaRedirected()
+        {
+            FacturaController controller = new FacturaController();
+
+            FacturaModel factura = new FacturaModel();
+            var resultado = (RedirectToRouteResult)controller.CompraRealizada();
+
+            Assert.IsTrue(resultado.RouteValues["action"].Equals("Index"));
+            Assert.IsTrue(resultado.RouteValues["controller"].Equals("Home"));
+        }
+    }
+}
---
+++ OrdenarPizza.Tests/Controllers/PizzaControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrdenarPizza.Controllers;
+
+namespace OrdenarPizza.Tests.Controllers
+{
+    [TestClass]
+    public class PizzaControllerTest
+    {
+        [TestMethod]
+        public void VerArmarPizzaIsNotNull()
+        {
+            PizzaController controller = new PizzaController();
+
+            ViewResult resultado = controller.VerArmarPizza() as ViewResult;
+
+            Assert.IsNotNull(resultado);
+        }
+    }
+}
